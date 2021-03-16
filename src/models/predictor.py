a4ed0407cc3e62ddf1e28a87d2eae721decb50da
Stellarator-X <admathur@iitk.ac.in>
2021-03-16 14:15:05 +0530

    rouge report

--- src/models/predictor.py
+++ src/models/predictor.py
@@ -186,11 +186,14 @@ class Translator(object):
 
         if (step != -1):
             rouges = self._report_rouge(gold_path, can_path)
-            self.logger.info('Rouges at step %d \n%s' % (step, rouge_results_to_str(rouges)))
+            self.logger.info(f'Rouges at step {step} \n {rouges}')
             if self.tensorboard_writer is not None:
-                self.tensorboard_writer.add_scalar('test/rouge1-F', rouges['rouge_1_f_score'], step)
-                self.tensorboard_writer.add_scalar('test/rouge2-F', rouges['rouge_2_f_score'], step)
-                self.tensorboard_writer.add_scalar('test/rougeL-F', rouges['rouge_l_f_score'], step)
+                try:
+                    self.tensorboard_writer.add_scalar('test/rouge1-F', rouges['rouge_1_f_score'], step)
+                    self.tensorboard_writer.add_scalar('test/rouge2-F', rouges['rouge_2_f_score'], step)
+                    self.tensorboard_writer.add_scalar('test/rougeL-F', rouges['rouge_l_f_score'], step)
+                except:
+                    print("Using a different rouge score report.")
 
     def _report_rouge(self, gold_path, can_path):
         self.logger.info("Calculating Rouge")
